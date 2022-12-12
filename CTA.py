88b984ca6ed57903ec0be9e1cc2e7ca2b01f93ec
nickgulson11 <nicholasgulson2021@u.northwestern.edu>
2022-12-12 16:16:37 -0600

    Changed L Stop

--- CTA.py
+++ CTA.py
@@ -5,6 +5,7 @@ train_api_key = "7edf9206960247dfad403efa90a3c88f"
 bus_api_key = "hyTi6EDZxyNFxe5nJefGbm4Ts"
 api_url = "http://lapi.transitchicago.com/api/1.0/"
 endpoints = json.loads('{"arrivals" : "ttarrivals.aspx"}')
+mapid = "40630" # Clark & Division
 
-x = requests.get(api_url + endpoints['arrivals'] + "?key=" + train_api_key + "&max=4&mapid=40360&outputType=JSON")
+x = requests.get(api_url + endpoints['arrivals'] + "?key=" + train_api_key + "&max=4&mapid=" + mapid + "&outputType=JSON")
 print(x.json())
