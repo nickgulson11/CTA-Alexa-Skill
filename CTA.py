094932fd1928df368fe3e5c0d4ff8da4a231ea40
nickgulson11 <nicholasgulson2021@u.northwestern.edu>
2022-12-12 16:39:28 -0600

    Structure Response

--- CTA.py
+++ CTA.py
@@ -7,5 +7,21 @@ api_url = "http://lapi.transitchicago.com/api/1.0/"
 endpoints = json.loads('{"arrivals" : "ttarrivals.aspx"}')
 mapid = "40630" # Clark & Division
 
-x = requests.get(api_url + endpoints['arrivals'] + "?key=" + train_api_key + "&max=4&mapid=" + mapid + "&outputType=JSON")
-print(x.json())
+response = requests.get(api_url + endpoints['arrivals'] + "?key=" + train_api_key + "&max=6&mapid=" + mapid + "&outputType=JSON")
+if response.status_code != 200:
+        print('error')
+else:        
+    trains = response.json()
+    trains = trains['ctatt']['eta']
+    northbound = []
+    southbound = []
+    for i in trains:
+        if i['destNm'] == 'Howard':
+            northbound.append(i['arrT'])
+        elif i['destNm'] == '95th/Dan Ryan':
+            southbound.append(i['arrT'])
+    print("Northbound Trains: ", northbound)
+    print("Southbound Trains: ", southbound)
+
+
+
